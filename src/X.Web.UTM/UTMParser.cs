8ed9228986a1c74350507f2fa38d6da474b3473f
Andrew <andrew.gubskiy@outlook.com>
2024-05-11 19:32:35 +0300

    Add UTMParser

---
+++ src/X.Web.UTM/UTMParser.cs
@@ -0,0 +1,14 @@
+namespace X.Web.UTM;
+
+public class UTMParser
+{
+    public UrchinTrackingModule Parse(string url)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public UrchinTrackingModule Parse(Uri url)
+    {
+        throw new NotImplementedException();
+    }
+}
